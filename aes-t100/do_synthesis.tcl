c3e9ebcb90e321b6c67259d3def4079a8011340e
Nathan <najohnson@umass.edu>
2021-04-26 00:20:13 -0400

    Added scan-chain generation to synthesis script

--- aes-t100/do_synthesis.tcl
+++ aes-t100/do_synthesis.tcl
@@ -37,13 +37,31 @@ max_area 0
 ## clock creation
 create_clock clk -period 10 -waveform {0 5.0}
 
+
+### Add test structures (scan cells)
+set test_default_scan_style multiplexed_flip_flop
+set_scan_configuration -chain_count 1
+create_test_protocol -infer_async -infer_clock
+
+## check design constraints
+dft_drc
+
 ## compile design to target library (OpenPDK45)
-compile
+compile -scan
+
+# connects all scan-enabled ff's together into scan-chain
+# note, it creates two new ports: test_si & test_se
+preview_dft
+insert_dft
+
+# Report stuff
+report_port
+dft_drc -coverage_estimate
+
+# write out test protocol (also needed by tetramax)
+write_test_protocol -output aes_test_protocol.spf
 
 ## write out gate level netlist
 write -hierarchy -f verilog -o aes_netlist.v
-
-## report port counts
-report_port
 
 quit
